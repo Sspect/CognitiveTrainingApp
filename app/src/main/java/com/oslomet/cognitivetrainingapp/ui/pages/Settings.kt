a0054add6d09b3e27b3d6f00012f7521838bb890
Sspect <bastian.stokke@gmail.com>
2025-04-10 17:32:06 +0200

    added cards to the game page

--- app/src/main/java/com/oslomet/cognitivetrainingapp/ui/pages/Settings.kt
+++ app/src/main/java/com/oslomet/cognitivetrainingapp/ui/pages/Settings.kt
@@ -1,17 +1,20 @@
 package com.oslomet.cognitivetrainingapp.ui.pages
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun SettingsPage(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize()
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        /*Spacer(modifier.padding(10.dp))*/
         Text(text = "Settings page")
     }
 }
